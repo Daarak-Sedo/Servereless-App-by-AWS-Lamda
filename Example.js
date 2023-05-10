68c62aac369630ceb61203997c9d1e0dffad7e16
bhuwan-crossml <132264068+bhuwan-CrossMl@users.noreply.github.com>
2023-05-10 14:22:06 +0530

    Create Example.js

---
+++ Example.js
@@ -0,0 +1,37 @@
+const AWS = require('aws-sdk');
+
+// Configure AWS Cognito Identity Provider
+const cognito = new AWS.CognitoIdentityServiceProvider({ region: 'YOUR_REGION' });
+
+// Example: List all users in a user pool
+const userPoolId = 'YOUR_USER_POOL_ID';
+const listUsersParams = {
+  UserPoolId: userPoolId,
+};
+
+cognito.listUsers(listUsersParams, (error, data) => {
+  if (error) {
+    console.error('Error listing users:', error);
+  } else {
+    console.log('Users:', data.Users);
+  }
+});
+
+// Example: Admin create user in a user pool
+const createUserParams = {
+  UserPoolId: userPoolId,
+  Username: 'john.doe',
+  TemporaryPassword: 'TempPassword123',
+  UserAttributes: [
+    { Name: 'email', Value: 'john.doe@example.com' },
+    // Add any additional user attributes here
+  ],
+};
+
+cognito.adminCreateUser(createUserParams, (error, data) => {
+  if (error) {
+    console.error('Error creating user:', error);
+  } else {
+    console.log('User created successfully:', data);
+  }
+});
